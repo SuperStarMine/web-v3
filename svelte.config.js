2e5edc30dde439ba8a845168834b263473028005
kazuemon <git@k6n.jp>
2021-11-03 21:51:30 +0900

    add: [WIP] necromance ページの追加

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,13 @@
 import preprocess from 'svelte-preprocess';
 import netlify from '@sveltejs/adapter-netlify';
+import importAssets from 'svelte-preprocess-import-assets';
+import { sequence } from './src/sequence.js';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: sequence([preprocess(), importAssets()]),
 
 	kit: {
 		adapter: netlify(),
